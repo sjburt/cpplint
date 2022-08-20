3579f19ee2b98dee171717dd19d94c971f7a4ec7
Thibault Kruse <thibault.kruse@gmx.de>
2022-08-20 15:50:42 +0200

    fix pylint warnings

--- cpplint.py
+++ cpplint.py
@@ -67,6 +67,7 @@ _valid_extensions = set([])
 __VERSION__ = '1.6.0'
 
 try:
+  #  -- pylint: disable=used-before-assignment
   xrange          # Python 2
 except NameError:
   #  -- pylint: disable=redefined-builtin
@@ -883,12 +884,14 @@ _line_length = 80
 _include_order = "default"
 
 try:
+  #  -- pylint: disable=used-before-assignment
   unicode
 except NameError:
   #  -- pylint: disable=redefined-builtin
   basestring = unicode = str
 
 try:
+  #  -- pylint: disable=used-before-assignment
   long
 except NameError:
   #  -- pylint: disable=redefined-builtin
